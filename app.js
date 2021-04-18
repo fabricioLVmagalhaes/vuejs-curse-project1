15efad0faaa4a8d6d4b1babd18b5974e2bda5d3a
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-04-18 14:23:48 -0300

    Adding a Special Attack

--- app.js
+++ app.js
@@ -8,18 +8,24 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
     monsterBarStyles() {
-        return {width: this.monsterHealth + '%'}
+      return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
-        return {width: this.playerHealth + '%'}
-    }
+      return { width: this.playerHealth + '%' };
+    },
+    mayUseSpecialAttack() {
+      console.log(this.currentRound % 3 !== 0);
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRamdomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -27,6 +33,12 @@ const app = Vue.createApp({
     attackPlayer() {
       const attackValue = getRamdomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRamdomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
   },
 });
